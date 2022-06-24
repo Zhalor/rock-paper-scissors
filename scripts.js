8f92793787faaaf6e081d13b950f029dd1dbab88
Jordan <uberbomb@yahoo.com>
2022-06-23 21:23:29 -0400

    Add styles, Javascript, GUI, and HTML structure
    Updated the rock, paper, scissors program to include DOM manipulation.
    Adjusted several functions and variables and created new ones to
    accommodate changes.
    Revised HTML and added basic styling.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
-// Creats variables for player win and computer win count. Sets them to 0.
-let pWin = 0;
-let cWin = 0;
+//Create variables used in program.
+let win = 0;
+let lose = 0;
+let tie = 0;
+const winDisplay = document.querySelector('#win');
+const loseDisplay = document.querySelector('#lose');
+const tieDisplay = document.querySelector('#tie');
+const container = document.querySelector('#container');
+const result = document.createElement('div');
+const playAgain = document.createElement('button');
+const btnRock = document.querySelector('#rock');
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
+
+//Set attributes and values to variables.
+winDisplay.textContent = win;
+loseDisplay.textContent = lose;
+tieDisplay.textContent = tie;
+playAgain.textContent = 'Play again?';
+playAgain.setAttribute('id', 'playAgain');
 
 //Function to generate a random number between 1 and 3
 //Converts number to the string rock, paper, or scissors based on number, respectively and 
@@ -16,56 +33,88 @@ const computerSelection = function() {
   }
 }
 
-//prompts player for selection of rock, paper, or scissors
-//converts selection to lowercase string and returns it to variable playerSelection.
-const playerSelection = function() {
-  selection = prompt("Rock, paper, or scissors?");
-  selection = selection.toLowerCase();
-  return selection;
-}
-
 //Computes the winner of rock paper scissors round by comparing the
 //player selection against the computer selection. Increments player win or computer win count by 1
 //based on result. Alerts the winner of the round or tie. Alerts if player makes an invaid selection.
-function playRound(pSelec, cSelec) {
-  console.log(pSelec, cSelec);
+function playRound(buttonId) {
+  pSelec = buttonId;
+  cSelec = computerSelection();
   switch (true){
     case (pSelec === "rock" && cSelec === "scissors"):
     case (pSelec === "paper" && cSelec === "rock"):
     case (pSelec === "scissors" && cSelec === "paper"):
-      alert(`You win! ${pSelec} beats ${cSelec}!`);
-      pWin++;
+      result.textContent = `You win! ${pSelec} beats ${cSelec}!`;
+      container.appendChild(result);
+      win++;
+      winDisplay.textContent = win;
+      if(win + lose + tie === 5){
+        showResult();
+      }
       break;
     case (pSelec === "rock" && cSelec === "paper"):
     case (pSelec === "paper" && cSelec === "scissors"):
     case (pSelec === "scissors" && cSelec === "rock"):
-      alert(`You lose! ${cSelec} beats ${pSelec}!`);
-      cWin++;
+      result.textContent = `You lose! ${cSelec} beats ${pSelec}!`;
+      container.appendChild(result);
+      lose++;
+      loseDisplay.textContent = lose;
+      if(win + lose + tie === 5){
+        showResult();
+      }
       break;
     case (pSelec === "rock" && cSelec === "rock"):
     case (pSelec === "paper" && cSelec === "paper"):
     case (pSelec === "scissors" && cSelec === "scissors"):
-      alert(`It's a tie! You both chose ${cSelec}.`);
+      result.textContent = `It's a tie! You both chose ${cSelec}.`;
+      container.appendChild(result);
+      tie++;
+      tieDisplay.textContent = tie;
+      if(win + lose + tie === 5){
+        showResult();
+      }
       break;
     default:
       alert("Please select rock, paper, or scissors.");
   }
 }
 
-//Runs 5 rounds of the game. Runs the player selection and computer selection functions.
-//The results are passed as variables to the playRound function. After 5 rounds, the player win and
-//computer win variables are compared and the winner or a tie is alerted.
-function game(){
-  for (let i = 0; i < 5; i++) {
-    playRound(playerSelection(), computerSelection());
+//Listener event for buttons. Passes id of button as argument.
+btnRock.addEventListener('click', function(){
+  playRound(this.id);
+});
+btnPaper.addEventListener('click', function(){
+  playRound(this.id);
+});
+btnScissors.addEventListener('click', function(){
+  playRound(this.id);
+});
+
+//Shows results after 5 games have been played and asks player to play again.
+showResult = function(){
+  if(win > lose){
+    result.textContent = `You win ${win} to ${lose}!`;
+    container.appendChild(result);
+  } else if (lose > win){
+    result.textContent = `You lose ${lose} to ${win}!`;
+    container.appendChild(result);
+  } else {
+    result.textContent = `It's a tie! ${win} to ${lose}`;
+    container.appendChild(result);
   }
-  if (pWin > cWin){
-    alert(`Congratulations You win ${pWin} to ${cWin}!`);
-  } else if (cWin > pWin){
-    alert(`Better luck next time. You lose ${cWin} to ${pWin}.`);
-  } else{
-    alert(`It's a tie! You both scored ${pWin}.`);
-  }
+  container.appendChild(playAgain);
+  playAgain.addEventListener('click', function(){
+    reset();
+  });
 }
 
-game();
+//Resets score and result.
+function reset(){
+  win = 0;
+  lose = 0;
+  tie = 0;
+  winDisplay.textContent = win;
+  loseDisplay.textContent = lose;
+  tieDisplay.textContent = tie;
+  container.removeChild(result);
+  container.removeChild(playAgain);
+}
